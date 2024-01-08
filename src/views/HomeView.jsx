aa5246c03f8166fcbdc1234d2ff25b19ce946f61
coconutpalmtreeisland <adorableness1215@naver.com>
2024-01-08 10:39:51 +0900

    24.01.08 배포

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -4,7 +4,6 @@ import Main from '../components/Main'
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
-import Section4 from '../components/Section4'
 import Section5 from '../components/Section5'
 import Loading from '../components/Loading'
 
@@ -23,7 +22,6 @@ const HomeView = () => {
                 <Section1 />
                 <Section2 />
                 <Section3 />
-                <Section4 />
                 <Section5 />
             </Main>
         </>
